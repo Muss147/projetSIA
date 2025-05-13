40dbe919c286bc3a9cdf405071bf8c7cd0fe3a5d
Moussa Fofana <mfofana@aguimaweb.com>
2025-05-13 00:34:34 +0000

    Module connexion : 10%

---
+++ src/Controller/AdminController/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\AdminController;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class DashboardController extends AbstractController
+{
+    #[Route('/', name: 'dashboard')]
+    public function dashboard(SessionInterface $session): Response
+    {
+        $session->set('menu', 'dashboard');
+        $session->set('subMenu', '');
+        return $this->render('dashboard.html.twig');
+    }
+}
